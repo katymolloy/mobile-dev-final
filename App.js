0f7af4d71ec6e19812864fa309b0274e8eebc432
FrederikFanshawe <f_sommerbrondum@fanshaweonline.ca>
2024-03-21 11:35:04 -0400

    Changes

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { GameEngine } from 'react-native-game-engine';
+import Physics from 'physics';
+import entities from "./entities";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={Images.Background}
+        style={styles.backgroundImage}
+        resizeMode="stretch"
+      />
+      <GameEngine
+        systems={[Physics]}
+        entities={entities()}
+        running={running}
+        style={styles.gameContainer}
+      >
+        {<StatusBar style="auto" hidden={true} />}
+      </GameEngine>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: Constants.SCREEN_WIDTH,
+    height: Constants.SCREEN_HEIGHT,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  gameContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
---
+++ physics.js
@@ -0,0 +1,9 @@
+import Matter, { Sleeping } from "matter-js";
+
+const Physics = (entities, { touches, time }) => {
+
+    let engine = entities.pysics.engine;
+
+    Matter
+
+}
